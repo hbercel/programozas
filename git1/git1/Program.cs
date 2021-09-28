21f19ac271c25a93d2db5c697dbf762becfcf957
horvath.bercel <horvath.bercel@M113-02>
2021-09-28 11:33:00 +0200

    órai munka

--- git1/git1/Program.cs
+++ git1/git1/Program.cs
@@ -34,36 +34,144 @@ namespace git1
             }
             //3. feladat
             Console.WriteLine("Ez a program eldönti hogy fagy van e kint vagy nem");
-            Console.ReadLine();
-            if (szam2 <= 0)
+            int szam3 = Convert.ToInt32(Console.ReadLine());
+            if (szam3 <= 0)
             {
-                Console.WriteLine("Fagy van kint {0} fok", szam2);
+                Console.WriteLine("Fagy van kint {0} fok", szam3);
             }
             else
             {
-                Console.WriteLine("nincs fagy kint {0} fok", szam2);
+                Console.WriteLine("nincs fagy kint {0} fok", szam3);
             }
             //4. feladat
             Console.WriteLine("Ez a program eldönti hogy a szám 30 és 40 között van e");
-            Console.ReadLine();
-            if ( szam2 < 40 && szam2 > 30 )
+            int szam4 = Convert.ToInt32(Console.ReadLine());
+            if ( szam4 < 40 && szam4 > -30 )
             {
-                Console.WriteLine("A {0} szám 30 és 40 között van", szam2);
+                Console.WriteLine("A {0} szám -30 és 40 között van", szam4);
             }
             else
             {
-                Console.WriteLine("A {0} szám nincsen 30 és 40 között", szam2);
+                Console.WriteLine("A {0} szám nincsen -30 és 40 között", szam4);
             }
-            Console.ReadLine();
-            if (szam2 < 16)
+            //5. feladat
+            Console.WriteLine("Ez a program megszoroza 10-el a 16 nál kissebb számokat és elosztja hárommal a 16 nál nagyobbakat ");
+            int szam5 = Convert.ToInt32(Console.ReadLine());
+            if (szam5 < 16)
+             {
+                 Console.WriteLine("A {0} szám kisseb mint 16 {1}",szam5, szam5*10);
+             }
+             else
+             {
+                Console.WriteLine("A {0} szám kissebb mint 16 {1} ", szam5,szam5 % 3);
+             }
+             Console.ReadLine();
+            //12. feladat
+            Console.WriteLine("Ez a program eldönti egy számról hogy melyik hónap");
+            int szam6 = Convert.ToInt32(Console.ReadLine());
+            switch (szam6)
             {
-                Console.WriteLine("A szám kisseb mint 16 {}", szam2*10);
+                case 1:
+                    Console.WriteLine("Január");
+                    
+                    break;
+                case 2:
+                    Console.WriteLine("Február");
+
+                    break;
+                case 3:
+                    Console.WriteLine("Március");
+
+                    break;
+                case 4:
+                    Console.WriteLine("Április");
+
+                    break;
+                case 5:
+                    Console.WriteLine("Május");
+
+                    break;
+                case 6:
+                    Console.WriteLine("Június");
+
+                    break;
+                case 7:
+                    Console.WriteLine("Julis");
+
+                    break;
+                case 8:
+                    Console.WriteLine("Augusztus");
+
+                    break;
+                case 9:
+                    Console.WriteLine("Szeptember");
+
+                    break;
+                case 10:
+                    Console.WriteLine("Október");
+
+                    break;
+                case 11:
+                    Console.WriteLine("November");
+
+                    break;
+                case 12:
+                    Console.WriteLine("December");
+
+                    break;
+
+                default:
+                    Console.WriteLine("Nem hónap szám");
+                    break;
+
+
             }
-            else
+            //13. feladat
+            Console.WriteLine("Ez a program eldönti egy számról hogy melyik évszak");
+            int szam7 = Convert.ToInt32(Console.ReadLine());
+            switch (szam7)
             {
-                Console.WriteLine("A {0} szám nincsen 30 és 40 között", szam2);
+                case 1:
+                case 2:
+                case 12:
+                    Console.WriteLine("Tél");
+                     
+                  break;
+
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("Tavasz");
+
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Nyár");
+
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Ősz");
+
+                    break;
+
+                default:
+                    Console.WriteLine("Nem évszak szám");
+                    break;
             }
+            //14. feladat
+            /*Console.WriteLine("Ez a program eldönti egy számról hogy hány jegyű");
+            int szam8 = Convert.ToInt32(Console.ReadLine());
+            switch (szam8)
+            {
+                case 1 - 10:
+                    Console.WriteLine("A szám egyjegyű");
+                  break;
+            }*/
             Console.ReadLine();
+
 
 
         }
