235e7464caaa0ff4968c57fe4b214b1559c3e908
horvath.bercel <horvath.bercel@M113-02>
2021-10-19 11:36:18 +0200

    zuz

---
+++ 20211019_2/20211019_2/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20211019_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //feladat1();
+            //feladat2();
+            //feladat3();
+            //feladat4();
+            feladat5();
+        }
+        static void feladat1()
+        {
+            Console.WriteLine("Kérem a téglalap oldalát");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            Console.WriteLine("A téglalap térfogata{0} és a felszíne{1}",a*b*c,2*(a*b+a*b+b*c));
+            Console.ReadLine();
+        }
+        static void feladat2()
+        {
+            Console.WriteLine("Kérek egy számot egy és hét között");
+            int szam = int.Parse(Console.ReadLine());
+            switch (szam)
+            {
+                case 1:
+                    Console.WriteLine("hétfő");
+                    break;
+                case 2:
+                    Console.WriteLine("Kedd");
+                    break;
+                case 3:
+                    Console.WriteLine("Szerda");
+                    break;
+                case 4:
+                    Console.WriteLine("Csütörtök");
+                    break;
+                case 5:
+                    Console.WriteLine("Péntek");
+                    break;
+                case 6:
+                    Console.WriteLine("Szombat");
+                    break;
+                case 7:
+                    Console.WriteLine("Vasárnap");
+                    break;
+
+                default:
+                    Console.WriteLine("nem jó szám");
+                    break;
+                    
+                    
+
+
+            }
+            Console.ReadLine();
+        }
+        static void feladat3()
+        {
+            Console.WriteLine("Kérek egy számot");
+            double szam = double.Parse(Console.ReadLine());
+            if (szam > -50 && 70 > szam)
+            {
+                Console.WriteLine("Benne van");
+            }
+            else
+            {
+                Console.WriteLine("Nincs benne");
+            }
+            Console.ReadLine();
+
+        }
+        static void feladat4()
+        {
+            for (int i = 120; i >= 39; i=i-15)
+            {
+                Console.WriteLine("{0}",i);
+            }
+            Console.ReadLine();
+        }
+        static void feladat5()
+        {
+            for (int i = 1; i < 40; i++)
+            {
+                if(i*9%7==0)
+                {
+                    Console.WriteLine("{0}",i*9);
+                }
+
+            }
+            Console.WriteLine();
+            int cv = 1;
+            while (cv < 41)
+            {
+                if (cv * 9 % 7 == 0)
+                {
+                    Console.WriteLine("{0}",cv*9);
+                }
+                cv++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ dolgozat/dolgozat/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dolgozat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+
+                //feladat.1
+                Console.WriteLine("Kérek egy téglatest három élének hosszát és megmondom a térfogatot és a felszínt");
+                int szamoa = Convert.ToInt32(Console.ReadLine());
+                int szamob = Convert.ToInt32(Console.ReadLine());
+                int szamoc = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("A téglatest felszine {0} ",2*(szamoa*szamob+szamoa*szamoc+szamob*szamoc));
+                Console.WriteLine("A téglatest térfogata {0}",szamoa*szamob*szamoc);
+                Console.ReadLine();
+                
+                //feladat.2
+               /*int szam2 = Convert.ToInt32(Console.ReadLine());
+               switch (szam2)
+               {
+                    case 1:
+                        Console.WriteLine("Hétfő");
+                        break;
+                    case 2:
+                        Console.WriteLine("Kedd");
+                        break;
+                    case 3:
+                        Console.WriteLine("Szerda");
+                        break;
+                    case 4:
+                        Console.WriteLine("Csütörtök");
+                        break;
+                    case 5:
+                        Console.WriteLine("Péntek");
+                        break;
+                    case 6:
+                        Console.WriteLine("Szombat");
+                        break;
+                    case 7:
+                        Console.WriteLine("Vasárnap");
+                        break;
+
+
+
+
+
+                }
+                Console.ReadLine();*/
+                //feladat.3
+                /*int szam2 = Convert.ToInt32(Console.ReadLine());
+                if (szam3 > -50 && szam3 < 70)
+                {
+                    Console.WriteLine("A {0} szám -50 és 70 között van", szam3);
+                }
+                else
+                {
+                    Console.WriteLine("A {0} szám nincs -50 és 70 között",szam3);
+                }
+                Console.ReadLine();*/
+                //feladat.4
+                /*for (int i = 40; i < 120; i= i+15)
+                {
+                    Console.WriteLine("{0}",i );
+                }
+                Console.ReadLine();*/
+
+
+
+            }
+        }
+    }
+}
